a185e2c3b1b6ddd7dc6fa12a221f705a55987afe
Yudi Yamane <yudiyamane@gmail.com>
2022-11-21 22:09:49 -0300

    feat: adiciona arquivo inicial

---
+++ labs/01-distributed-word-counter/1-worker/dwc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <mpi/mpi.h>
+
+// mpirun $(pwd)/dwc
+
+int main(int argc, char **argv) {
+    int nprocs;
+
+    MPI_Init(&argc, &argv);
+    printf("hello world\n");
+    MPI_Finalize();
+}
