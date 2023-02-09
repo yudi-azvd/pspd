9c3232363e765fa3f2ce685bfdb74a353538eec9
Yudi Yamane <yudiyamane@gmail.com>
2023-02-09 18:30:32 -0300

    chore: adiciona script de download

--- labs/04-openmp-mpi/upload.sh
+++ labs/04-openmp-mpi/upload.sh
@@ -1,9 +1,14 @@
-<< 'COMMENT'
-- Upload para chococino de todos o códigos C e do Makefile
-- Compila
-- Executa
+# << 'COMMENT'
+# - Upload para chococino de todos o códigos C e do Makefile
+# - Compila
+# - Executa
 
-Seria bom um download.sh para baixar os atefatos (csv?, imagens)
+# Seria bom um download.sh para baixar os atefatos (csv?, imagens)
 
-em um plot.py pra plotar os csv
-COMMENT
+# em um plot.py pra plotar os csv
+# COMMENT
+
+final_dir="/pspd/04-openmp-mpi"
+
+files="fractal Makefile ordena *.h"
+scp -P 13508 -r ${files}  a160149410@chococino.naquadah.com.br:~${final_dir}
