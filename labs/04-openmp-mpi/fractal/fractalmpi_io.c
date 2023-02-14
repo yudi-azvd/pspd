cd881935f9bf4ad7443df3ae154a7f8564e2bfdf
Yudi Yamane <yudiyamane@gmail.com>
2023-02-14 15:41:51 -0300

    feat: adiciona código questão 2

--- labs/04-openmp-mpi/fractal/fractalmpi_io.c
+++ labs/04-openmp-mpi/fractal/fractalmpi_io.c
@@ -1,14 +1,18 @@
+#include "../job_division_mpi.h"
 #include <math.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #define OUTFILE "out_julia_normal.bmp"
+// É o que está  em write_bmp_header
+#define HEADER_OFFSET (54)
 
 int compute_julia_pixel(int x, int y, int width, int height, float tint_bias, unsigned char* rgb) {
     if ((x < 0) || (x >= width) || (y < 0) || (y >= height)) {
         fprintf(stderr, "Invalid (%d,%d) pixel coordinates in a %d x %d image\n", x, y, width, height);
         return -1;
     }
-
     // "Zoom in" to a pleasing view of the Julia set
     float X_MIN = -1.6, X_MAX = 1.6, Y_MIN = -0.9, Y_MAX = +0.9;
     float float_y = (Y_MAX - Y_MIN) * (float)y / height + Y_MIN;
@@ -33,20 +37,16 @@ int compute_julia_pixel(int x, int y, int width, int height, float tint_bias, un
     rgb[0] = (num_iter == 0 ? 200 : -500.0 * pow(tint_bias, 1.2) * pow(color_bias, 1.6));
     rgb[1] = (num_iter == 0 ? 100 : -255.0 * pow(color_bias, 0.3));
     rgb[2] = (num_iter == 0 ? 100 : 255 - 255.0 * pow(tint_bias, 1.2) * pow(color_bias, 3.0));
-    float avg = (rgb[0] + rgb[1] + rgb[2]) / 3;
-    rgb[0] = avg;
-    rgb[1] = avg;
-    rgb[2] = avg;
 
     return 0;
 }
 
-int write_bmp_header(FILE* f, int height, int width) {
-    unsigned int row_size_in_bytes = height * 3 + ((height * 3) % 4 == 0 ? 0 : (4 - (height * 3) % 4));
+int write_bmp_header(MPI_File file, int width, int height) {
+    unsigned int row_size_in_bytes = width * 3 + ((width * 3) % 4 == 0 ? 0 : (4 - (width * 3) % 4));
 
     // Define all fields in the bmp header
     char id[2] = "BM";
-    unsigned int filesize = 54 + (int)(row_size_in_bytes * width * sizeof(char));
+    unsigned int filesize = 54 + (int)(row_size_in_bytes * height * sizeof(char));
     short reserved[2] = {0, 0};
     unsigned int offset = 54;
 
@@ -54,7 +54,7 @@ int write_bmp_header(FILE* f, int height, int width) {
     unsigned short planes = 1;
     unsigned short bits = 24;
     unsigned int compression = 0;
-    unsigned int image_size = height * width * 3 * sizeof(char);
+    unsigned int image_size = width * height * 3 * sizeof(char);
     int x_res = 0;
     int y_res = 0;
     unsigned int ncolors = 0;
@@ -62,46 +62,122 @@ int write_bmp_header(FILE* f, int height, int width) {
 
     // Write the bytes to the file, keeping track of the
     // number of written "objects"
-    size_t ret = 0;
-    ret += fwrite(id, sizeof(char), 2, f);
-    ret += fwrite(&filesize, sizeof(int), 1, f);
-    ret += fwrite(reserved, sizeof(short), 2, f);
-    ret += fwrite(&offset, sizeof(int), 1, f);
-    ret += fwrite(&size, sizeof(int), 1, f);
-    ret += fwrite(&height, sizeof(int), 1, f);
-    ret += fwrite(&width, sizeof(int), 1, f);
-    ret += fwrite(&planes, sizeof(short), 1, f);
-    ret += fwrite(&bits, sizeof(short), 1, f);
-    ret += fwrite(&compression, sizeof(int), 1, f);
-    ret += fwrite(&image_size, sizeof(int), 1, f);
-    ret += fwrite(&x_res, sizeof(int), 1, f);
-    ret += fwrite(&y_res, sizeof(int), 1, f);
-    ret += fwrite(&ncolors, sizeof(int), 1, f);
-    ret += fwrite(&importantcolors, sizeof(int), 1, f);
+    int count = 0, ret = 0;
+    MPI_Status status;
+    MPI_File_write(file, id, 2, MPI_CHAR, &status);
+    MPI_Get_count(&status, MPI_CHAR, &count);
+    ret += count;
+    MPI_File_write(file, &filesize, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, reserved, 2, MPI_SHORT, &status);
+    MPI_Get_count(&status, MPI_SHORT, &count);
+    ret += count;
+    MPI_File_write(file, &offset, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &size, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &width, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &height, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &planes, 1, MPI_SHORT, &status);
+    MPI_Get_count(&status, MPI_SHORT, &count);
+    ret += count;
+    MPI_File_write(file, &bits, 1, MPI_SHORT, &status);
+    MPI_Get_count(&status, MPI_SHORT, &count);
+    ret += count;
+    MPI_File_write(file, &compression, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &image_size, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &x_res, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &y_res, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &ncolors, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
+    MPI_File_write(file, &importantcolors, 1, MPI_INT, &status);
+    MPI_Get_count(&status, MPI_INT, &count);
+    ret += count;
 
     // Success means that we wrote 17 "objects" successfully
     return (ret != 17);
 }
 
 int main(int argc, char* argv[]) {
-    int n;
+    int n, nprocs, workers, rank;
+    int range[2];
     int area = 0, width = 0, height = 0, local_i = 0;
-    FILE* output_file;
     unsigned char *pixel_array, *rgb;
+    double t1, t2;
+    FILE* output_file;
 
-    if ((argc <= 1) | (atoi(argv[1]) < 1)) {
-        fprintf(stderr, "Entre 'N' como um inteiro positivo! \n");
-        return -1;
+    MPI_File file;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if ((argc <= 1) || (atoi(argv[1]) < 1)) {
+        if (rank == 0) {
+            fprintf(stderr, "Entre 'N' como um inteiro positivo! \n");
+        }
+        MPI_Finalize();
+        return 1;
     }
+
+    int file_mode = MPI_MODE_WRONLY | MPI_MODE_CREATE;
+    int ierr = MPI_File_open(MPI_COMM_WORLD, OUTFILE, file_mode, MPI_INFO_NULL, &file);
+    if (ierr) {
+        if (rank == 0) {
+            fprintf(stderr, "Could not open file %s\n", OUTFILE);
+        }
+        MPI_Finalize();
+        return 2;
+    }
+
+    workers = nprocs;
     n = atoi(argv[1]);
     height = n;
     width = 2 * n;
     area = height * width * 3;
     pixel_array = calloc(area, sizeof(unsigned char));
     rgb = calloc(3, sizeof(unsigned char));
-    printf("Computando linhas de pixel %d até %d, para uma área total de %d\n", 0, n - 1, area);
 
-    for (int i = 0; i < n; i++)
+    if (write_bmp_header(file, width, height)) {
+        if (rank == 0) {
+            fprintf(stderr, "Failed writing header\n");
+        }
+
+        MPI_Finalize();
+        return -3;
+    }
+
+    divide_jobs_and_offsets(n, workers);
+    int line_start = job_offsets[rank];
+    int line_jump = job_division[rank];
+    int line_end = line_start + line_jump;
+    int byte_count = line_jump * width * BYTES_PER_PIXEL;
+    int byte_offset = line_start * width * BYTES_PER_PIXEL;
+
+    // if (rank == 0) {
+    //     printf("Computando linhas de pixel %d até %d, para uma área total de %d bytes\n", 0, n - 1, area);
+    // print_job_division(workers);
+    // print_job_offsets(workers);
+    // }
+
+    t1 = MPI_Wtime();
+    local_i += byte_offset;
+    for (int i = line_start; i < line_end; i++)
         for (int j = 0; j < width * 3; j += 3) {
             compute_julia_pixel(j / 3, i, width, height, 1.0, rgb);
             pixel_array[local_i++] = rgb[0];
@@ -109,12 +185,21 @@ int main(int argc, char* argv[]) {
             pixel_array[local_i++] = rgb[2];
         }
 
+    // printf("PID %d | start: %d, end: %d, jump: %d, bytes: %d, local_i: %d, byte offs: %d\n", rank, line_start, line_end, line_jump, byte_count, local_i,
+    //        byte_offset);
+
+    // MPI_Request req;
+    // MPI_File_iwrite_at(file, HEADER_OFFSET + byte_offset, pixel_array + byte_offset, byte_count, MPI_UNSIGNED_CHAR, &req);
+    MPI_File_write_at(file, HEADER_OFFSET + byte_offset, pixel_array + byte_offset, byte_count, MPI_UNSIGNED_CHAR, MPI_STATUS_IGNORE);
+
+    t2 = MPI_Wtime();
     free(rgb);
-    output_file = fopen(OUTFILE, "w");
-    write_bmp_header(output_file, width, height);
-    // escreve o array no arquivo
-    fwrite(pixel_array, sizeof(unsigned char), area, output_file);
-    fclose(output_file);
     free(pixel_array);
+
+    if (rank == 0)
+        printf("N = %d, Time elapsed %f\n", n, t2 - t1);
+
+    MPI_File_close(&file);
+    MPI_Finalize();
     return 0;
 }
